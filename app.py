8201ec4d87c4c6ce68cd03bd492ab36f4384dc03
whyashthakker <nynansnh@gmail.com>
2023-07-02 17:30:58 +0530

    open sourced

--- app.py
+++ app.py
@@ -22,6 +22,10 @@ with st.sidebar:
 
 def main():
     st.header("Compliance Questions?")
+    OPENAI_API_KEY = st.text_input(
+        "Enter your API key", type="password"
+    )  # Add this line
+
     load_dotenv()
 
     pdf = st.file_uploader("Upload a PDF file", type="pdf")
@@ -29,63 +33,65 @@ def main():
     if pdf is not None:  # check if a file was uploaded
         pdf_reader = PdfReader(pdf)
         st.write(pdf.name)
- 
+
         text = ""
 
         for page in pdf_reader.pages:
             text += page.extract_text()
 
-        text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, 
-                                                       chunk_overlap=200, 
-                                                       length_function = len
-                                                       )
-        
-        chunks = text_splitter.split_text(text = text)
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=1000, chunk_overlap=200, length_function=len
+        )
 
-        #st.write(chunks)
+        chunks = text_splitter.split_text(text=text)
 
+        # st.write(chunks)
 
-    #EMBEDDINGS
+        # EMBEDDINGS
 
-        embeddings = OpenAIEmbeddings()
-        VectorStore = FAISS.from_texts(chunks, embedding = embeddings)
+        embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
+        VectorStore = FAISS.from_texts(chunks, embedding=embeddings)
         store_name = pdf.name[:-4]
 
         if os.path.exists(f"{store_name}.pk1"):
             with open(f"{store_name}.pk1", "rb") as f:
                 VectorStore = pickle.load(f)
-            #st.write("VectorStore loaded from file.")
+            # st.write("VectorStore loaded from file.")
         else:
-            embeddings = OpenAIEmbeddings()
-            VectorStore = FAISS.from_texts(chunks, embedding = embeddings)
+            embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
+            VectorStore = FAISS.from_texts(chunks, embedding=embeddings)
             with open(f"{store_name}.pk1", "wb") as f:
                 pickle.dump(VectorStore, f)
 
-            #st.write("VectorStore created and saved to file.")
+            # st.write("VectorStore created and saved to file.")
 
-    #ACCEPT USER QUESTIONS / QUERY
+        # ACCEPT USER QUESTIONS / QUERY
 
         query = st.text_input("Let's ask questions :)")
         st.write(query)
 
         if query:
             docs = VectorStore.similarity_search(query=query, k=3)
-            llm = OpenAI(model_name='gpt-3.5-turbo', max_tokens=100)
-            chain = load_qa_chain(llm = llm, chain_type = "stuff")
+            llm = OpenAI(
+                model_name="gpt-3.5-turbo",
+                max_tokens=100,
+                openai_api_key=OPENAI_API_KEY,
+            )
+            chain = load_qa_chain(llm=llm, chain_type="stuff")
 
             with get_openai_callback() as cb:
-                response = chain.run(input_documents=docs,question=query)
+                response = chain.run(input_documents=docs, question=query)
                 print(cb)
+                st.write(cb)
                 st.write(response)
 
-            #st.write(docs)
+            # st.write(docs)
 
-
-    #st.write(text)
+    # st.write(text)
 
     else:
         st.write("Please upload a PDF file.")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
- 
