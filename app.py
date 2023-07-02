52e20aba222443ff7e9e6835d4970cdd2c915be2
whyashthakker <nynansnh@gmail.com>
2023-07-02 17:58:26 +0530

    pdf chat

--- app.py
+++ app.py
@@ -13,11 +13,19 @@ import os
 
 # sidebar
 with st.sidebar:
-    st.write("## Talk to PDFs")
-    st.markdown("### Ask questions about Compliance, Accounting, and anything else.")
+    st.write("## AI-Powered PDF Assistant")
+    st.markdown(
+        "### Query your Compliance, Accounting, and other Business PDFs like never before."
+    )
+    st.markdown(
+        "Our smart PDF assistant uses state-of-the-art AI technology to extract and analyze text from your PDFs. It allows you to ask complex questions and get precise answers based on the information contained in your documents. Ideal for professionals in the field of Compliance, Accounting, Legal, and anyone else who handles data-heavy PDFs in their work."
+    )
 
     add_vertical_space(2)
-    st.write("## About")
+    st.write("## About AI-Powered PDF Assistant")
+    st.markdown(
+        "Our platform leverages the power of OpenAI's language model, offering a new level of interaction with your PDFs. We transform static documents into an interactive knowledge base where queries can be made seamlessly. From accounting principles to compliance regulations, our AI-Powered PDF Assistant will provide concise answers extracted from the content of your uploaded files."
+    )
 
 
 def main():
@@ -52,8 +60,6 @@ def main():
 
         chunks = text_splitter.split_text(text=text)
 
-        # st.write(chunks)
-
         # EMBEDDINGS
 
         embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
@@ -63,14 +69,11 @@ def main():
         if os.path.exists(f"{store_name}.pk1"):
             with open(f"{store_name}.pk1", "rb") as f:
                 VectorStore = pickle.load(f)
-            # st.write("VectorStore loaded from file.")
         else:
             embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
             VectorStore = FAISS.from_texts(chunks, embedding=embeddings)
             with open(f"{store_name}.pk1", "wb") as f:
                 pickle.dump(VectorStore, f)
-
-            # st.write("VectorStore created and saved to file.")
 
         # ACCEPT USER QUESTIONS / QUERY
 
@@ -91,11 +94,6 @@ def main():
                 print(cb)
                 st.write(cb)
                 st.write(response)
-
-            # st.write(docs)
-
-    # st.write(text)
-
     else:
         st.write("Please upload a PDF file.")
 
