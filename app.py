5c3700c598b771fede06b0a56f90e478193ccd74
whyashthakker <nynansnh@gmail.com>
2023-07-02 18:33:20 +0530

    warning

--- app.py
+++ app.py
@@ -40,7 +40,9 @@ def main():
             "**⚠️ Please note:** Your API key is not stored or sent anywhere. Entire code is open source, see this: https://github.com/whyashthakker/chatpdf, drop a ⭐️",
             unsafe_allow_html=True,
         )
-        api_key_confirmation = st.checkbox("I understand and accept.")
+        api_key_confirmation = st.checkbox(
+            "Please note, the API code will cost you money for using OpenAI API. We're using GPT-3.5 Turbo Model. Please take a note of this. I understand and accept."
+        )
         if not api_key_confirmation:
             return
 
